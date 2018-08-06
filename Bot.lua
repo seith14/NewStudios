0232e734ffbf64afbb89df2d3e3e54c7f265ef17
seith14 <42125680+seith14@users.noreply.github.com>
2018-08-05 21:40:21 -0500

    Update Bot.lua

--- Bot.lua
+++ Bot.lua
@@ -1,70 +1,16 @@
-const discordiaModule = require('discordia')
-const lua = require('lua')
-const Client = discordiaModule.Client()
-const pr = "!"
-const start = false
-const maxwarns = 4
-const warns = {}
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-Client:on('ready', function()
-	const channel = Client:getChannel('459817720150491149')
-  channel:send('Bot Updated')
-end)
+client.on('ready', () => {
+	console.log('I am ready!');
+	message.reply('Bot Loaded');
+});
 
-Client:on('memberJoin', function (mem)
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
-end)
-
-Client:on('memberLeave', function (mem)
-	
-end)
-
-function checker(msg, stuff, i)
-	const plr = stuff:sub(#pr+6, #stuff)
-			print(i.id)
-		const memb = msg.guild:getMember(i.id)
-		if i.name:sub(1, #plr) == plr and msg.member.highestRole.name > memb.highestRole.name then
-			return true
-		end
-end
-
-
-Client:on('messageCreate', function(msg)
-	const channel = Client:getChannel('459817720150491149')
-	const stuff = msg.content
-	print(msg.author.id)
-	if msg.member.highestRole.name >= "Co-Owner" then
-	if stuff:sub(1,#pr+4) == pr.."warn" then
-		for _, i in pairs(Client.users) do
-		const checked = checker(msg, stuff, i)
-		if checked then
-		table.insert(warns, i.name)
-			local amm = maxwarns
-			for _, o in pairs(warns) do
-				if o == i.name then
-					amm = amm - 1
-				end
-				if amm <= 0 then
-					local mem = msg.guild:getMember(i.id)
-					mem.guild:kickUser(mem.user, "Warned Multiple times")
-				end
-			end
-		msg.channel:send(i.name.." You have been warned!  "..amm.." more warning(s) and you will be kicked from server")
-		end
-	end
-		elseif stuff:sub(1,#pr+4) == pr.."kick" then
-			for _, i in pairs(Client.users) do
-			const checked = checker(msg, stuff, i)
-			if checked then
-				local mem = msg.guild:getMember(i.id)
-				mem.guild:kickUser(mem.user, "Behavior")
-			end
-	end
-	end
-end
-	
-	print(msg.member.highestRole.name)
-	print(#Client.users)
-end)
-
-Client:run("Bot NDYwOTkzNjk0NTI3NjUxODQy.DhNZ6A.679IZl1xHrmO394p0bJZVzC2Qnc")
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
